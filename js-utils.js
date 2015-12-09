f9be5ff5d5bfb7b14ac5fdb530040981c9d6a933
Leandro <leito.vm3@hotmail.com>
2015-12-09 10:43:24 -0300

    Updated docs

--- js-utils.js
+++ js-utils.js
@@ -4,7 +4,11 @@ var utils = utils || (function () {
 
     var helper = helper || {};
 
-    // Check if the object has a property, if so returns true
+    /**
+     * Check if the object has a property
+     * @param   {object}  object the object to check if empty
+     * @returns {boolean} true if the object has no properties
+     */
     helper.isEmptyObject = function (object) {
         for (var property in object) {
             if (object.hasOwnProperty(property)) {
@@ -15,23 +19,40 @@ var utils = utils || (function () {
         return true;
     };
 
-    // Check if a variable is empty, null, undefined or blank .
+    /**
+     * Check if a variable is empty, null, undefined or blank .
+     * @param   {any type} variable the variable to check if empty
+     * @returns {boolean}  true if the object is empty
+     */
     helper.isEmpty = function (variable) {
-        return (variable === null || typeof variable === "undefined" || variable === {} || this.isEmptyObject(variable) || variable === "");
+        return (variable === null || typeof variable === "undefined" || variable === "" || variable === {} || this.isEmptyObject(variable));
     };
 
-    // Check if 2 strings are case insensitive equals
+    /**
+     * Check if 2 strings are case insensitive equals
+     * @param   {string}  firstString  one string
+     * @param   {string}  secondString another string
+     * @returns {boolean} true if the 2 strings are case insensitive equals
+     */
     helper.isStringInsensitiveEquals = function (firstString, secondString) {
         return firstString.toUpperCase() === secondString.toUpperCase();
     };
 
-    // Cut the last character from a string
+    /**
+     * Cut the last character from a string
+     * @param   {string} string the string to cut the last character
+     * @returns {string} the given string without the last character
+     */
     helper.cutLastChar = function (string) {
         return string.slice(0, -1);
     };
 
-    // Transform a request parameter json object to string
-    // JSON format: {key : "value", key : "value"} ==> to String: key=value&key=value
+    /**
+     * Transform a request parameter json object to string
+     * JSON format: {key : "value", key : "value"} ==> to String: key=value&key=value
+     * @param   {object} paramsObject the object with the paremeters
+     * @returns {string} the given parameters as URL format string
+     */
     helper.paramsToString = function (paramsObject) {
         var stringParams = "";
         for (var property in paramsObject) {
@@ -40,18 +61,28 @@ var utils = utils || (function () {
             }
         }
 
-        // Cut last & character
+        // Cut last "&" character
         return (this.cutLastChar(stringParams)).trim();
     };
 
-    // Returns the extension from a filename, e.g. exampleImg.png
+    /**
+     * Returns the extension from a filename
+     * e.g. exampleImg.png => file extension: png
+     * @param   {string} fileName the file name with the extension
+     * @returns {string} the file extension
+     */
     helper.getFileExtension = function (fileName) {
         var extension = fileName.split('.');
 
         return extension[extension.length - 1];
     };
 
-    // Returns the filename from a full path url, e.g /resources/images/thatimage.png
+    /**
+     * Returns the file name from a full path url
+     * e.g /resources/images/thatimage.png => file name: thatimage.png
+     * @param   {string} filePath the whole file path
+     * @returns {string} the file name
+     */
     helper.getFileNameFromPath = function (filePath) {
         var splitResult = filePath.split("/");
 
@@ -59,4 +90,5 @@ var utils = utils || (function () {
     };
 
     return helper;
+
 }());
